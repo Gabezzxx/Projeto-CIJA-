77424092995dec69938c4212e67e302e41f40a95
Thiago Abraão <thiagocontaazr123@gmail.com>
2026-07-26 15:21:47 -0300

    removendo arquivos com erros

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'], 
-  plugins: [
-    '@babel/plugin-transform-classes' // Adiciona o plugin caso o projeto realmente precise dele
-  ],
-  ignore: [
-    "./node_modules" // < Impede que o Babel tente compilar dependências externas
-  ]
-};
